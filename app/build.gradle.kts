e84f8677f24c8c230074d392db5e9143b20ccb04
d0x-dev <tstarkx103@gmail.com>
2026-09-01 20:24:38 +0530

    chore: prepare version to be bumped to 6.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 182
-        versionName = "6.0.10"
+        versionName = "6.9.-1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GOOGLE_API_KEY", googleApiKey.asBuildConfigString())
         buildConfigField("String", "STATS_API_KEY", statsApiKey.asBuildConfigString())
